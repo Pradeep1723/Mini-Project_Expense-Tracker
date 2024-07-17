3e83ca441daca39d9e2d0eabac27e64dbb83c7a6
Pradeep1723 <bhatpradeep1723@gmail.com>
2024-07-17 12:23:42 +0530

    Code improvements in server and minor change in db connection

--- backend/db/dbConnection.js
+++ backend/db/dbConnection.js
@@ -4,8 +4,10 @@ const db = async () => {
 	try {
 		await mongoose.connect(process.env.MONGO_CONNECTION_STRING);
 		console.log('Connected to the DB');
+		return true;
 	} catch (error) {
 		console.error(`DB Connection Error: ${error}`);
+		return false;
 	}
 };
 
--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,24 @@ require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes/route');
+const { db } = require('./db/dbConnection');
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
+const startServer = async () => {
+    const isDbConnected = await db();
+    if (!isDbConnected) {
+        console.error('Failed to connect to DB. Server not started.');
+    } else {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port: ${PORT}`);
+        });
+    }
+};
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-});
+startServer();
